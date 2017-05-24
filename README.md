47337805dca7837da5cb5245dfc5bfe217935d20
Edward Schaefer <edwardaschaefer@gmail.com>
2017-05-23 22:56:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+This application uses Google Maps API and Knockout Javascript framwork to explore 
+murals around Baltimore City, Maryland. The Socrata Open Data API is used to 
+retrieve data about murals around Baltimore from Open Baltimore, an open 
+database with information about Baltimore. This data is retrieved 
+asynchronously via JQuery's $.Ajax method and devlivered in JSON format. The 
+data is saved into the Knockout model, as well as a Google Maps API markers 
+object. Each mural is assigned an id which keeps the data from each source in 
+correspondence. When a list item or marker is clicked, more information about 
+the mural is displayed. Responsive CSS is used so the application displays of 
+a variery of browsers.
+
+#How to Run the Application
+---------------------------
+To run the application, simply clone the repository, then open index.html. 
+Enter search terms in the upper left corner. Click items in the list or 
+markers on the right for more information.
+
+#List of Files
+--------------
+
+	*js/app: The application
+	*js/lib/knockout-3.4.2.js: Knockout Library
+	*css/styles.css: CSS styling
+	*index.html: The HTML page the application is rendered on
+	*README.md: this readme file
---
+++ js/app.js
@@ -0,0 +1,125 @@
+//Initialize map object
+var map;
+
+//Create placemarkers array to use in multiple functions to have control over the number of places that show.
+var markers = [];
+
+//Define parameters for retrieving data from Open Baltimore/Socrata Open Data API
+$.ajax({
+    url: "https://data.baltimorecity.gov/resource/x3eq-9sua.json",
+    type: "GET",
+    data: {
+      "$limit" : 5000,
+      "$$app_token" : "IAPCDodkEyfP95b6c7eJLut59"
+    }
+//Prepare the data for Google Maps API and Knockout 
+}).done(function(data) {
+//Define the prototype for the Google Maps InfoWindow object 
+    var largeInfoWindow = new google.maps.InfoWindow();
+    for(var i = 0; i < data.length; i++) {
+//Add a unique ID for each object in the array
+        data[i].id = i;
+//Create a new marker object and add the data to it
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(data[i].location_1.coordinates[1], data[i].location_1.coordinates[0]),
+            firstname: data[i].artistfirstname,
+            lastname: data[i].artistlastname,
+            address: data[i].location,
+            zip: data[i].location_1_zip,
+            year: data[i].year,
+            id: i,
+            icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+            map: map
+        });
+//Add an event listener (google API) that displays the infowindow
+        marker.addListener('click', function(){
+            populateInfoWindow(this, largeInfoWindow);
+        });
+//Add location to array for KO list
+        viewModel.locations.push(data[i]);
+//Add marker to the global markers object
+        markers.push(marker);
+    }
+//If the data isn't retrieved from the server, send the error message to the KO observable
+}).fail(function() {
+    viewModel.ajaxFail('Failed to retrieve data via API')
+})
+
+//Model for Knockout
+var Model = {
+
+};
+
+//View Model for Knockout
+var AppViewModel = function() {
+    var self = this;
+//Locations for KO list
+    self.locations = ko.observableArray();
+//Search query observable which is updated every keystroke
+    self.query = ko.observable('');
+//Search results observable which returns any location with an artist last name that matches the query
+    self.filtered = ko.computed(function(){
+//Takes the locations array and returns any entries with a matching index 
+        return ko.utils.arrayFilter(self.locations(), function(location){
+//Returns a value only if a matching value is found (-1 is returned if there is none)
+            return location.artistlastname.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
+        });
+    });
+//The ID of the mural in question, uesd so the clicked list mural matches the clicked marker and vice versa
+    self.selectedMural = ko.observable();
+//Message to display if AJAX call fails
+    self.ajaxFail = ko.observable('')
+};
+
+//Apply bindings to view
+viewModel = new AppViewModel();
+ko.applyBindings(viewModel);
+
+//Selects the mural on the map, as if it was clicked
+function selectMural(mural) {
+    google.maps.event.trigger(markers[mural.id], 'click');
+}
+
+//Initialize the map for Google Maps API
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 39.310000, lng: -76.610000},
+        zoom: 13
+    });
+}
+
+//Populates the InfoWindow when the marker is clicked.
+function populateInfoWindow(marker, infowindow) {
+//Check to make sure the InfoWindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>Name: ' + marker.firstname + ' ' + marker.lastname + '<br>' +
+                'Address: ' + marker.address + ' ' + marker.zip + '<br>' +
+                'Year: ' + marker.year + '</div>');
+//Open the InfoWindow
+        infowindow.open(map, marker);
+//Make sure the marker property is cleared if the InfoWindow is closed.
+        infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
+        });
+//Changes marker's icon from default
+        marker.setIcon('http://maps.google.com/mapfiles/ms/icons/purple-dot.png');
+//Sets the KO observable to match the mural's ID so it is highlighted on the list
+        viewModel.selectedMural(marker.id);
+    }
+}
+
+//Hides all the markers, then displays the ones which match the search results
+function markerUpdate() {
+    //Loops through the markers and hides them
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    };
+    //Gets the length of search results
+    length = viewModel.filtered().length;
+    //Loops through the markers and redisplay them based on IDs from search results
+    for (i = 0; i < length; i++) {
+        x = viewModel.filtered()[i].id;
+        markers[x].setMap(map);
+    };
+}
